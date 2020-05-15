303311d4a2a3fcabe93fc75ac220578ee4e8a73a
Pau Joan <pregonipaw@hotmail.com>
2020-05-15 16:23:00 +0200

    Added. upload images.

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const mongoose = require('mongoose');
-const usersRoute = require('./routes/users');
 const bodyParser = require('body-parser');
 require('dotenv/config');
-var cors = require('cors');
+const cors = require('cors');
+
 // CORS ENABLED
 app.use(cors());
 // BODY PARSER
 app.use(bodyParser.json());
 
 // CONNECT DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const db = mongoose.connection;
 
-
+app.use('/public/images', express.static(__dirname + '/public/images'))
 // ROUTES
+const usersRoute = require('./routes/users');
+const uploadRoute = require('./routes/upload');
 app.use('/api/users', usersRoute);
+app.use('/api/upload', uploadRoute);
 
-app.get('/', (req, res) => {
-   res.send("<h1>My api</h1>")
-})
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
---
+++ routes/upload.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const verify = require('./verifyToken');
+const Users = require('../models/Users');
+const multer = require('multer');
+require('dotenv/config');
+
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      cb(null, './public/images')
+   },
+   filename: (req, file, cb) => {
+      cb(null, file.originalname)
+   }
+});
+
+const fileFilter = (req, file, cb) => {
+   if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+      cb(null, true);
+   } else {
+      cb(null, false);
+   }
+}
+// const upload = multer({ dest: 'public/images/' })
+const upload = multer({
+   storage: storage, limits: {
+      fileSize: 500000
+   },
+   fileFilter: fileFilter
+})
+
+
+router.put('/', verify, upload.single('file'), async (req, res) => {
+   const file = req.file;
+   const userId = req.headers.userid;
+
+   try {
+      const updatedUser = await Users.findByIdAndUpdate({ _id: userId }, {
+         $set: {
+            avatarUrl: process.env.URL + 'public/images/' + file.originalname
+         }
+      }, {
+         new: true
+      });
+
+      res.status(200).json({ avatarUrl: updatedUser.avatarUrl });
+   } catch (error) {
+      res.status(400).json({ message: `Can't upload your file.` });
+      console.log(error)
+   }
+})
+
+
+module.exports = router;
